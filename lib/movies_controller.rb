3248db3adccb5a45ef780c97234654e5d0f2f896
Benji Dalton <daylightsavings@gmail.com>
2015-12-05 23:44:11 +1300

    add search controller and tmdb api

--- lib/movies_controller.rb
+++ lib/movies_controller.rb
@@ -1,4 +1,5 @@
 require_relative "./movie.rb"
+require "pstore"
 
 class MoviesController
   def all
@@ -9,7 +10,8 @@ class MoviesController
     
   end
 
-  def add
+  def add(title)
+    movie = Search.query(title)
     puts "adding..."
   end
 
